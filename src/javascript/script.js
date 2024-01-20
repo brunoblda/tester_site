a3b3e2f6fb5530e120bcc9ca0613b0117f621180
brunoblda <brunoblda@gmail.com>
2024-01-20 19:50:06 -0300

    fix: adding no cors

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,5 +1,5 @@
 async function get_cookies() {
-  const response = await fetch("https://gtdedge-hello-world.booking.com/echo");
+  const response = await fetch("https://gtdedge-hello-world.booking.com/echo", { mode: 'no-cors'});
   const movies = await response.json();
   console.log(movies);
 }
