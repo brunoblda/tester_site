a88dff5fe89332c09cd97c281205e347e244c5ed
brunoblda <brunoblda@gmail.com>
2024-01-20 19:57:21 -0300

    fix: ajuste no fetch

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,5 +1,18 @@
 async function get_cookies() {
-  const response = await fetch("https://gtdedge-hello-world.booking.com/echo", { mode: 'no-cors'});
-  const movies = await response.json();
-  console.log(movies);
-}
+  //const response = await fetch("https://gtdedge-hello-world.booking.com/echo", { mode: 'no-cors'});
+  //const movies = await response.json();
+  //console.log(movies);
+
+  return fetch('https://gtdedge-hello-world.booking.com/echo', {    
+      method: 'GET',    
+      crossorigin: true,    
+      mode: 'no-cors',       
+    })    
+      .then((res) => res.json())    
+      .then((data) => {    
+        console.log(data);    
+      })    
+      .catch((error) => {    
+        console.error(error);    
+      });    
+  };   
