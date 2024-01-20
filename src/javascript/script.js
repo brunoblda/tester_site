89fae7c1435705316a799199d9d3f85705c42260
brunoblda <brunoblda@gmail.com>
2024-01-20 20:26:20 -0300

    fix: ajustes do cors

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,21 +1,22 @@
 async function get_cookies() {
-  //const response = await fetch("https://gtdedge-hello-world.booking.com/echo", { mode: 'no-cors'});
-  //const movies = await response.json();
-  //console.log(movies);
+  const response = await fetch("https://gtdedge-hello-world.booking.com/echo", {method: "GET",crossorigin: true});
+  const movies = await response.json();
+  console.log(movies);
+}
 
-  return fetch('https://gtdedge-hello-world.booking.com/echo', {    
-      method: 'GET',    
-      crossorigin: true,    
-      mode: 'no-cors',       
-    })    
-      .then((res) => res.json())    
-      .then((data) => {    
-        console.log(data);    
-      })    
-      .catch((error) => {    
-        console.error(error);    
-      });    
-  };   
+  //return fetch('https://gtdedge-hello-world.booking.com/echo', {    
+      //method: 'GET',    
+      //crossorigin: true,    
+      //mode: 'no-cors',       
+    //})    
+      //.then((res) => res.json())    
+      //.then((data) => {    
+        //console.log(data);    
+      //})    
+      //.catch((error) => {    
+        //console.error(error);    
+      //});    
+  //};   
 
 iframe.onload = function() {
    var content = iframe.contentDocument || iframe.contentWindow.document;
